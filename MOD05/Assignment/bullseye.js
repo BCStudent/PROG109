32ce7ce8371f7342af85305368bfec78e776ae4e
BCStudent <35323130+BCStudent@users.noreply.github.com>
2018-02-09 22:29:56 -0800

    Update bullseye.js

--- MOD05/Assignment/bullseye.js
+++ MOD05/Assignment/bullseye.js
@@ -1,25 +1,52 @@
-var person = prompt("Please Choose a Number:", "1"); 
+@import url(http://fonts.googleapis.com/css?family=Wellfleet);
 
-var table = person;             // Unit of table
-var operator = 'multiplication'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '<h2>Multiplication Table</h2>';              // Message
+body {
+	background-color: #523620;
+	color: #fff;
+	font-family: 'Wellfleet', times, serif;
+	font-size: 150%;
+	margin: 0px 0px 0px 0px;}
 
+h1 {
+	background-image: url('../images/bullseye-logo.png');
+	background-repeat: no-repeat;
+	text-indent: 100%;
+	white-space: nowrap;
+	overflow: hidden;
+	height: 109px;
+	width: 643px;
+	margin: 40px auto;}
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
-}
+#teacher {
+	float:right;
+	margin:0px 30px 0px 0px;}
+#teacher2 {
+	float:right;
+	margin:135px 30px 0px 0px;}
 
-// Write the message into the page
-var el = document.getElementById('blackboard');
-el.innerHTML = msg;
+#page1, #page2 {
+	background-color: #fecc6f;
+	height: 596px;
+	padding: 10px;
+	min-width: 779px;}
+#page2 {
+	height: 730px;}
+
+#answer {
+	border: 12px double #fff;
+	color: #523620;
+	text-align: left;
+	padding: 20px;
+	margin: 70px auto 10px auto;
+	width: 250px;
+	text-align: center;}
+
+#blackboard {
+	background-color: #425a5a;
+	border: 25px solid #523620;
+	border-radius: 20px;
+	padding: 40px 20px;
+	margin: 0px auto;
+	height: 370px;
+	width: 600px;
+	text-align: center;}
