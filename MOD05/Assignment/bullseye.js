9e4467fb06ab3d50546561b04088755445485370
BCStudent <35323130+BCStudent@users.noreply.github.com>
2018-02-09 22:39:58 -0800

    Update bullseye.js

--- MOD05/Assignment/bullseye.js
+++ MOD05/Assignment/bullseye.js
@@ -5,11 +5,6 @@ var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';              // Message
 
-var table = 3;             // Unit of table
-var operator = 'addition'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '';              // Message
-
 if (operator === 'addition') {
   // Do addition
   while (i < 11) {
