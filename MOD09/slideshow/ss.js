ba581b3070d14f8e260b1daf5f5272aeae68acc7
BCStudent <35323130+BCStudent@users.noreply.github.com>
2018-03-09 15:01:19 -0800

    Update ss.js

--- MOD09/slideshow/ss.js
+++ MOD09/slideshow/ss.js
@@ -34,3 +34,11 @@ var previousButton = document.getElementById("previous");
 
 previousButton.addEventListener("click",back,false);
 nextButton.addEventListener("click",next,false); 
+
+function autoSlide(){
+if (document.getElementById("auto").checked)
+ next(); 
+}
+
+
+setInterval(autoSlide,2000); // Next
